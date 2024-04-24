78f11b8107352d74f72ce2fdbef541e64b816238
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-24 14:11:14 -0300

    map é um tipo de coleção q itera sobre dados do banco e api. muito usado no mercado

--- Sintaxe, colecoes e dinamismo/maps/bin/enums.dart
+++ Sintaxe, colecoes e dinamismo/maps/bin/enums.dart
@@ -1,3 +1,33 @@
 void main() {
-  
+  //maps
+  Map<String, dynamic> regPreco = {};
+  regPreco["SP"] = 1200;
+  regPreco["RJ"] = 1500;
+
+  dynamic variavel = regPreco["SP"];
+
+  print(variavel);
+
+  var pessoa = Pessoa("Pablo", 21, true);
+
+  var pessoaMap = pessoa.toMap();
+
+  print(pessoaMap);
+  print(pessoaMap["idade"]);
+}
+
+class Pessoa{
+  String nome;
+  int idade;
+  bool estaAutenticada;
+
+  Pessoa(this.nome, this.idade, this.estaAutenticada);
+
+  Map<String, dynamic> toMap(){
+     return {
+      'nome': nome,
+      'idade': idade,
+      'estaAutenticada': estaAutenticada,
+     };
+  }
 }

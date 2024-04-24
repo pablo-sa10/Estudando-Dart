e4b573eb0e6938ee20ee710dd62dfd42f2c4867c
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-24 11:22:34 -0300

    iniciando maps

---
+++ Sintaxe, colecoes e dinamismo/colecções/bin/enums.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<String> destinos = [];
+  String destino = "Rio de janeiro";
+  String destino2 = "São Paulo";
+
+  destinos.add(destino);
+  destinos.add(destino2);
+  //print(destinos);
+
+  List<int> num = [1, 2, 3, 4, 1, 2];
+  Set<int> numSemRepetir = {};
+
+  numSemRepetir.addAll(num);
+
+  print(numSemRepetir);
+
+  Set<String> mano = resgistrarDestino(destino, destino2);
+  print(mano);
+}
+
+Set<String>resgistrarDestino(String destino, String destino2) {
+  Set<String> resgistroVisitados = <String>{};
+  resgistroVisitados.add(destino);
+  resgistroVisitados.add(destino2);
+  return resgistroVisitados;
+}

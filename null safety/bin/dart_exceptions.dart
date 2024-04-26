2197cc3b3a6fba634ae648d4f63598e3016490ec
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-26 11:17:29 -0300

    null safety finalizado

--- null safety/bin/dart_exceptions.dart
+++ null safety/bin/dart_exceptions.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'controllers/bank_controller.dart';
 import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
 void main() {
+  testeNumeroAleatorio();
   // Criando o banco
   BankController bankController = BankController();
 
@@ -23,7 +26,7 @@ void main() {
         idSender: "Kako", idReceiver: "Ricarth", amount: 100);
 
     // Observando resultado
-    if(result){
+    if (result) {
       print("transação concluída com sucesso");
     }
   } on SenderIdInvalidException catch (e) {
@@ -35,6 +38,29 @@ void main() {
   } on ReceiverNotAuthenticatedException catch (e) {
     print("o usuário ${e.idReceiver} não está autenticado");
   } on SenderBalanceLowerThanAmountException catch (e) {
-    print("o usuário ${e.idSender} tentou enviar ${e.amount}, porém sua conta possui apenas ${e.balance}");
+    print(
+        "o usuário ${e.idSender} tentou enviar ${e.amount}, porém sua conta possui apenas ${e.balance}");
   }
+}
+
+//criar uma função q gera numero aleatorios
+void testeNumeroAleatorio() {
+  Account? usuarioNull = Account(name: "Pablo Moura", balance: 10000, isAuthenticated: true);
+
+  Random rng = Random();
+
+  // for(int i = 0; i < 10; i++){
+  //   int numero = rng.nextInt(20);
+  //   print(numero);
+  // }
+  int chance = rng.nextInt(10);
+  print("numero gerado $chance");
+  if (chance <= 5) {
+    usuarioNull.createdAt = DateTime.now(); 
+            
+  }
+  print(usuarioNull.runtimeType);
+  print(usuarioNull.balance);
+  //print(usuarioNull.createdAt.day)
+  print(usuarioNull.createdAt);
 }
--- null safety/bin/models/account.dart
+++ null safety/bin/models/account.dart
@@ -2,11 +2,13 @@ class Account {
   String name;
   double balance;
   bool isAuthenticated;
+  DateTime? createdAt;
 
   Account(
       {required this.name,
       required this.balance,
-      required this.isAuthenticated});
+      required this.isAuthenticated,
+      this.createdAt});
 
   editBalance({required value}) {
     balance = balance + value;

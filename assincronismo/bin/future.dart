ce353736e9dde65f1bc50874fbbcb98172271e0e
Pablo <pablomoura2016@gmail.com>
2024-04-28 11:41:17 -0300

    entendendo o exception com future

--- assincronismo/bin/future.dart
+++ assincronismo/bin/future.dart
@@ -1,17 +1,40 @@
-void main(){
+import 'dart:ffi';
 
-  Future myFuture = Future(() {
-    print("de volta para o futuro");
-    return 21;
-  }).then((value) => print(value));
+void main() {
+  // Future myFuture = Future(() {
+  //   print("de volta para o futuro");
+  //   return 21;
+  // }).then((value) => print(value));
 
-  futureFunc().then((value) => print('minha função vale: $value'));
+  // futureFunc()
+  //     .then((value) => print('minha função vale: $value'))
+  //     .onError((error, stackTrace) => print("um erro aconteceu"))
+  //     .whenComplete(() => print("acabou o future"));
 
+  myFutureError(2, 1).then((value) => print(value));
   print("terminou a main");
 }
 
-Future<int> futureFunc()async{
+Future<int> futureFunc() async {
   print("eu tenho uma função do futuro");
   await Future.delayed(Duration(seconds: 5));
   return 12;
+}
+
+Future<int> myFutureError(int a, int b) async{
+  try{
+    if(a > b){
+      throw Exception();
+    }
+    print("função funcional");
+    await Future.delayed(Duration(seconds: 3));
+    return 42;
+  }
+  catch(e){
+    print('erro: $e');
+    return 42;
+  }
+  finally{
+    print("o futuro terminou");
+  }
 }
